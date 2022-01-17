b4b2bdbd43a3256b40d4fec2911048246c7b5c82
yungyungGwon <gywn7724@gmail.com>
2022-01-17 23:12:23 +0900

    theme test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,46 +10,12 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50%{
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1.5s linear infinite;
-  ${Emoji}:hover {
-      font-size: 98px;
-    }
-  }
-`;
-
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="P">🙃</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
