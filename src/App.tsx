4a65e39f3350a9e1bb1dd4ad3c38efc5424cf6c1
yungyungGwon <gywn7724@gmail.com>
2022-01-18 22:01:15 +0900

    TS interface test

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="white" bgColor="teal"/>
+      <Circle text="im here" bgColor="tomato"/>
+    </div>
+  );
+}
+
+export default App;
