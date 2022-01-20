4afdf113407a9bfb954c5f9c01a079c3ffea156a
yungyungGwon <gywn7724@gmail.com>
2022-01-20 15:41:45 +0900

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
 function App() {
+  
   return (
-    <div>
-      <Circle borderColor="white" bgColor="teal"/>
-      <Circle text="im here" bgColor="tomato"/>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {ThemeProvider} from "styled-components";
 import App from "./App";
+import { lightTheme,darkTheme  } from "./thems";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <ThemeProvider theme={darkTheme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
